890817d1a982530c9f1fb1d8415c157b6f9b66f0
Witold Andreasik <w64042@student.wsiz.edu.pl>
2023-04-15 10:28:51 +0200

    review changes

--- main.py
+++ main.py
@@ -8,13 +8,27 @@ m.add_child(folium.Marker(location=[50.04908088464327, 21.981612615229974], popu
 m.add_child(folium.Marker(location=[49.949890921270715, 22.058874020398942], popup="nasz marker: Kielnarowa ", icon=folium.Icon("green")))
 m.add_child(folium.Marker(location=[50.08951265233697, 21.520494332301062], popup="nasz marker: Paszczyna", icon=folium.Icon("blue")))
 
-with open("Volcanoes.txt", mode='r') as v:
-        for volcano in v.readlines()[1:]:
-            params = volcano.strip().split(',')
-            folium.map.Marker([float(params[-2]), float(params[-1])], popup=params[3], icon=folium.Icon("orange")).add_to(m)
+# simple way
+# with open("Volcanoes.txt", mode='r') as v:
+#         for volcano in v.readlines()[1:]:
+#             params = volcano.strip().split(',')
+#             folium.map.Marker([float(params[-2]), float(params[-1])], popup=params[3], icon=folium.Icon("orange")).add_to(m)
+# m.save('map.html')
+#
+
+# pandas
+data = pd.read_csv('Volcanoes.txt')
+# data series, i loc, 2 wiersze data frame
+print(data['LAT'])
+print(data['NAME'].iloc[3])
+
+lat = list(data['LAT'])
+lon = list(data['LON'])
+names = list(data['NAME'])
+
+for lat, lon, name in zip(lat, lon, names):
+    folium.map.Marker([float(lat), float(lon)], popup=[name], icon=folium.Icon("orange")).add_to(m)
 m.save('map.html')
 
 
-# pandas
-# data = pd.read_cs('Volcanoes.txt')
-# data series, i loc, 2 wiersze data frame
+
